716cb3fa6fa3a07e98e93d6d8cabba3a4fc22515
preeti453 <yadavpreeti30748@gmail.com>
2025-07-02 14:32:39 +0530

    add express file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const serveIndex = require("serve-index");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Enable directory listing for /songs
+app.use(
+  "/songs",
+  serveIndex(path.join(__dirname, "public/songs"), { icons: true })
+);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
