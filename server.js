50db48922571aaf8861842965510aeaa915947bb
preeti453 <yadavpreeti30748@gmail.com>
2025-07-02 16:29:29 +0530

    updated server file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const serveIndex = require("serve-index");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
